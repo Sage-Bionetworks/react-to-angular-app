f7d47cec2b3590d97b28bf3135a74560694c3325
tschaffter <thomas.schaffter@gmail.com>
2021-10-01 17:33:25 -0700

    Trying to import UserCard, issues with svg assets from SRC

--- src/app/hello/hello.component.ts
+++ src/app/hello/hello.component.ts
@@ -1,9 +1,11 @@
 import { Component, Input, OnInit } from '@angular/core';
 import * as React from 'react';
 import { SynapseClient } from 'synapse-react-client';
-import MarkdownSynapse, {
-  MarkdownSynapseProps,
-} from 'synapse-react-client/dist/containers/MarkdownSynapse';
+// import MarkdownSynapse, {
+//   MarkdownSynapseProps,
+// } from 'synapse-react-client/dist/containers/MarkdownSynapse';
+import UserCard, { UserCardProps } from 'synapse-react-client/dist/containers/UserCard';
+
 import { SynapseContextProvider } from 'synapse-react-client/dist/utils/SynapseContext';
 
 @Component({
@@ -17,10 +19,10 @@ export class HelloComponent implements OnInit {
   constructor() {}
 
   ngOnInit(): void {
-    const wikiProps: MarkdownSynapseProps = {
-      ownerId: this.ownerId || 'syn18142975', // testLegoMan
-    };
-    const wikiContent = React.createElement(MarkdownSynapse, wikiProps);
+    // const wikiProps: MarkdownSynapseProps = {
+    //   ownerId: this.ownerId || 'syn18142975', // testLegoMan
+    // };
+    // const wikiContent = React.createElement(MarkdownSynapse, wikiProps);
     // const props = {
     //   synapseContext: {
     //     accessToken: undefined,
@@ -28,6 +30,25 @@ export class HelloComponent implements OnInit {
     //     utcTime: SynapseClient.getUseUtcTimeFromCookie(),
     //   },
     // };
+
+    const userCardProps: UserCardProps = {
+      ownerId: this.ownerId || '273950', // testLegoMan
+      size: 'AVATAR',
+      avatarSize: 'LARGE'
+    };
+
+    const props = {
+      synapseContext: {
+        accessToken: undefined,
+        isInExperimentalMode: false,
+        utcTime: SynapseClient.getUseUtcTimeFromCookie(),
+      },
+    };
+
+  //   ReactDOM.render(
+  //     React.createElement(SynapseContextProvider, props, wikiContent),
+  //     this.containerRef.nativeElement
+  // );
 
   }
 }
